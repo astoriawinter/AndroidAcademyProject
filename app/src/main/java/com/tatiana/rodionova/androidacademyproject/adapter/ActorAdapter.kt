7a5b97a31b0f63c92aa629a7cd1ebccdc8e597e4
Tatiana Rodionova <tatiana.rodionova@emlid.com>
2020-11-21 16:15:16 +0300

    add ActorAdapter

---
+++ app/src/main/java/com/tatiana/rodionova/androidacademyproject/adapter/ActorAdapter.kt
@@ -0,0 +1,55 @@
+package com.tatiana.rodionova.androidacademyproject.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.tatiana.rodionova.androidacademyproject.R
+import com.tatiana.rodionova.androidacademyproject.model.Actor
+import kotlin.properties.Delegates
+
+class ActorAdapter : RecyclerView.Adapter<ActorAdapter.ActorViewHolder>() {
+
+    private val diffCallback = object : DiffUtil.ItemCallback<Actor>() {
+
+        override fun areContentsTheSame(oldItem: Actor, newItem: Actor): Boolean =
+            oldItem.resource == newItem.resource
+
+        override fun areItemsTheSame(oldItem: Actor, newItem: Actor): Boolean =
+            oldItem.name == newItem.name
+    }
+    private val differ = AsyncListDiffer(this, diffCallback)
+
+    var list: List<Actor> by Delegates.observable(listOf()) { _, _, list ->
+        differ.submitList(list)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ActorViewHolder =
+        ActorViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.item_actor, parent, false)
+        )
+
+    override fun getItemCount(): Int = differ.currentList.size
+
+    override fun onBindViewHolder(holder: ActorViewHolder, position: Int) {
+        holder.bind(differ.currentList[position])
+    }
+
+    class ActorViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val name = itemView.findViewById<TextView>(R.id.name)
+        private val picture = itemView.findViewById<ImageView>(R.id.picture)
+
+        fun bind(item: Actor) {
+            Glide.with(itemView.context)
+                    .load(item.resource)
+                    .into(picture)
+
+            name.text = item.name
+        }
+    }
+}
