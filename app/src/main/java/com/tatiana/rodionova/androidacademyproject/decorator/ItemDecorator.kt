95d57bfb57ae13fcc5727e343c39f69aea7d4718
Tatiana Rodionova <tatiana.rodionova@emlid.com>
2020-11-29 15:23:02 +0300

    upate ItemDecorator to support spanCount

---
+++ app/src/main/java/com/tatiana/rodionova/androidacademyproject/decorator/ItemDecorator.kt
@@ -0,0 +1,20 @@
+package com.tatiana.rodionova.androidacademyproject.decorator
+
+import android.graphics.Rect
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class ItemDecorator(private val margin: Int, private val spanCount: Int) : RecyclerView.ItemDecoration() {
+    override fun getItemOffsets(
+        outRect: Rect, view: View,
+        parent: RecyclerView, state: RecyclerView.State
+    ) {
+        super.getItemOffsets(outRect, view, parent, state)
+
+        val position = parent.getChildAdapterPosition(view)
+        val column = position % spanCount
+
+        outRect.left = column * margin / spanCount
+        outRect.right = margin - (column + 1) * margin / spanCount
+    }
+}
