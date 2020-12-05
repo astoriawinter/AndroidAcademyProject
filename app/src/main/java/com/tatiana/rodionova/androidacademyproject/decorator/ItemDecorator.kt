144ea1b0e1254cd06ba5673779c6f0a314fb56b5
Tatiana Rodionova <tatiana.rodionova@emlid.com>
2020-12-05 16:51:26 +0300

    update item decorator with margin

--- app/src/main/java/com/tatiana/rodionova/androidacademyproject/decorator/ItemDecorator.kt
+++ app/src/main/java/com/tatiana/rodionova/androidacademyproject/decorator/ItemDecorator.kt
@@ -4,17 +4,23 @@ import android.graphics.Rect
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 
-class ItemDecorator(private val margin: Int, private val spanCount: Int) : RecyclerView.ItemDecoration() {
+class ItemDecorator(private val resource: Int, private val spanCount: Int) : RecyclerView.ItemDecoration() {
+
     override fun getItemOffsets(
         outRect: Rect, view: View,
         parent: RecyclerView, state: RecyclerView.State
     ) {
         super.getItemOffsets(outRect, view, parent, state)
 
+        val margin = parent.context.resources.getDimension(resource).toInt()
         val position = parent.getChildAdapterPosition(view)
         val column = position % spanCount
 
         outRect.left = column * margin / spanCount
         outRect.right = margin - (column + 1) * margin / spanCount
+
+        if (position >= spanCount) {
+            outRect.top = margin
+        }
     }
 }
