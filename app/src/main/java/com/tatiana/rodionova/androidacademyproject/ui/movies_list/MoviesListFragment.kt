ac16645786f3d2c6fe3b34e287729f9a664bab0d
Tatiana Rodionova <tatiana.rodionova@emlid.com>
2020-12-19 19:01:05 +0300

    update MoviesListFragment with render logic

--- app/src/main/java/com/tatiana/rodionova/androidacademyproject/ui/movies_list/MoviesListFragment.kt
+++ app/src/main/java/com/tatiana/rodionova/androidacademyproject/ui/movies_list/MoviesListFragment.kt
@@ -7,7 +7,6 @@ import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.activity.OnBackPressedCallback
 import androidx.core.view.isVisible
-import androidx.core.widget.NestedScrollView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.GridLayoutManager
