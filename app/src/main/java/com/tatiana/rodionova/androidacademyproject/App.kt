299e5987e3673d14cd6ab8bc0db377fa0a079be8
Tatiana Rodionova <tatiana.rodionova@emlid.com>
2020-12-27 14:56:48 +0300

    add application with koin init

---
+++ app/src/main/java/com/tatiana/rodionova/androidacademyproject/App.kt
@@ -0,0 +1,19 @@
+package com.tatiana.rodionova.androidacademyproject
+
+import android.app.Application
+import com.tatiana.rodionova.androidacademyproject.di.repositoryModule
+import com.tatiana.rodionova.androidacademyproject.di.viewModelModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin {
+            androidContext(this@App)
+            modules(viewModelModule, repositoryModule)
+        }
+    }
+}
