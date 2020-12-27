5655cde875496953469b3539a1200b74d1b518d0
Tatiana Rodionova <tatiana.rodionova@emlid.com>
2020-12-27 14:59:47 +0300

    add MovieDetailsViewModel & update MovieDetailsViewFragment

---
+++ app/src/main/java/com/tatiana/rodionova/androidacademyproject/ui/movie_details/MovieDetailsViewModel.kt
@@ -0,0 +1,39 @@
+package com.tatiana.rodionova.androidacademyproject.ui.movie_details
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.tatiana.rodionova.androidacademyproject.data.MovieRepository
+import com.tatiana.rodionova.androidacademyproject.model.Movie
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+sealed class MovieDetailsState {
+    object Loading : MovieDetailsState()
+    object Error : MovieDetailsState()
+    class Success(val movie: Movie?) : MovieDetailsState()
+}
+
+class MovieDetailsViewModel(private val movieRepository: MovieRepository) : ViewModel() {
+
+    private val state: MutableLiveData<MovieDetailsState> by lazy {
+        MutableLiveData<MovieDetailsState>()
+    }
+
+    init {
+        state.postValue(MovieDetailsState.Loading)
+    }
+
+    fun getMovieById(id: Long): LiveData<MovieDetailsState> {
+        viewModelScope.launch {
+            try {
+                val movie = withContext(coroutineContext) { movieRepository.getMovieById(id) }
+                state.postValue(MovieDetailsState.Success(movie = movie))
+            } catch (e: Exception) {
+                state.postValue(MovieDetailsState.Error)
+            }
+        }
+        return state
+    }
+}
