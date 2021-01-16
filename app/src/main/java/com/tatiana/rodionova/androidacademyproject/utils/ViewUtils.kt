a7f70272f6e78dcc3daf42c1af919ee7e80edf9d
Tatiana Rodionova <tatiana.rodionova@emlid.com>
2021-01-16 22:33:49 +0300

    update formatting in viewUtils

--- app/src/main/java/com/tatiana/rodionova/androidacademyproject/utils/ViewUtils.kt
+++ app/src/main/java/com/tatiana/rodionova/androidacademyproject/utils/ViewUtils.kt
@@ -10,22 +10,23 @@ import androidx.fragment.app.Fragment
 import com.tatiana.rodionova.androidacademyproject.R
 
 fun TextView.setGradient(
-        colors: IntArray = intArrayOf(
-                ContextCompat.getColor(context, R.color.gallery),
-                ContextCompat.getColor(context, R.color.silver),
-                ContextCompat.getColor(context, R.color.white))
+    colors: IntArray = intArrayOf(
+        ContextCompat.getColor(context, R.color.gallery),
+        ContextCompat.getColor(context, R.color.silver),
+        ContextCompat.getColor(context, R.color.white)
+    )
 ) {
     val paint: TextPaint = paint
     val width = paint.measureText(text.toString())
 
     val textShader: Shader = LinearGradient(
-            0f, 0f, width, textSize,
-            colors, floatArrayOf(0f, 0.47f, 1f), Shader.TileMode.CLAMP
+        0f, 0f, width, textSize,
+        colors, floatArrayOf(0f, 0.47f, 1f), Shader.TileMode.CLAMP
     )
     paint.shader = textShader
 }
 
 inline fun <T : Fragment> T.withArgs(argsBuilder: Bundle.() -> Unit): T =
-        this.apply {
-            arguments = Bundle().apply(argsBuilder)
-        }
+    this.apply {
+        arguments = Bundle().apply(argsBuilder)
+    }
