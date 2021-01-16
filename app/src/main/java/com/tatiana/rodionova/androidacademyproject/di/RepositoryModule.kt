f950765c2c44b32b88639bdafd6b35c798dabb36
Tatiana Rodionova <tatiana.rodionova@emlid.com>
2021-01-16 22:31:18 +0300

    add network module & add new dependency to repositoryModule

--- app/src/main/java/com/tatiana/rodionova/androidacademyproject/di/RepositoryModule.kt
+++ app/src/main/java/com/tatiana/rodionova/androidacademyproject/di/RepositoryModule.kt
@@ -1,8 +1,8 @@
 package com.tatiana.rodionova.androidacademyproject.di
 
-import com.tatiana.rodionova.androidacademyproject.data.MovieRepository
+import com.tatiana.rodionova.androidacademyproject.data.repository.MovieRepository
 import org.koin.dsl.module
 
 val repositoryModule = module {
-    single { MovieRepository(get()) }
+    single { MovieRepository(get(), get()) }
 }
