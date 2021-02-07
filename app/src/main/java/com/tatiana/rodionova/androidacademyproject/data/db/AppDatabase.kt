ed327e5952712b243c212f05d47efe389d6e5796
Tatiana Rodionova <tatiana.rodionova@emlid.com>
2021-02-07 20:47:03 +0300

    update AppDatabase with new version

--- app/src/main/java/com/tatiana/rodionova/androidacademyproject/data/db/AppDatabase.kt
+++ app/src/main/java/com/tatiana/rodionova/androidacademyproject/data/db/AppDatabase.kt
@@ -6,15 +6,12 @@ import com.tatiana.rodionova.androidacademyproject.data.db.dao.ActorDao
 import com.tatiana.rodionova.androidacademyproject.data.db.dao.GenreDao
 import com.tatiana.rodionova.androidacademyproject.data.db.dao.MovieDao
 import com.tatiana.rodionova.androidacademyproject.data.db.dao.MovieDetailedDao
-import com.tatiana.rodionova.androidacademyproject.data.db.entity.ActorEntity
-import com.tatiana.rodionova.androidacademyproject.data.db.entity.GenreEntity
-import com.tatiana.rodionova.androidacademyproject.data.db.entity.MovieDetailedEntity
-import com.tatiana.rodionova.androidacademyproject.data.db.entity.MovieEntity
+import com.tatiana.rodionova.androidacademyproject.data.db.entity.*
 
 @Database(
-    entities = [MovieEntity::class, MovieDetailedEntity::class, GenreEntity::class, ActorEntity::class],
-    version = 1,
-    exportSchema = true
+    entities = [MovieEntity::class, MovieDetailedEntity::class, GenreEntity::class, ActorEntity::class, MovieWithGenreCrossRef::class],
+    version = 2,
+    exportSchema = false
 )
 abstract class AppDatabase : RoomDatabase() {
 
