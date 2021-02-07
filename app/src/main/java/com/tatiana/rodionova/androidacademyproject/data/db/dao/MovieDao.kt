2fa1d697933209fb9325292c599d0efe097e93f8
Tatiana Rodionova <tatiana.rodionova@emlid.com>
2021-02-07 20:41:23 +0300

    update movie & movie detailed dao
    - replaced update with insert

--- app/src/main/java/com/tatiana/rodionova/androidacademyproject/data/db/dao/MovieDao.kt
+++ app/src/main/java/com/tatiana/rodionova/androidacademyproject/data/db/dao/MovieDao.kt
@@ -3,19 +3,25 @@ package com.tatiana.rodionova.androidacademyproject.data.db.dao
 import androidx.room.*
 import com.tatiana.rodionova.androidacademyproject.data.db.entity.MovieEntity
 import com.tatiana.rodionova.androidacademyproject.data.db.entity.MovieWithGenre
+import com.tatiana.rodionova.androidacademyproject.data.db.entity.MovieWithGenreCrossRef
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.distinctUntilChanged
 
 @Dao
 interface MovieDao {
-    @Insert
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertMovies(movies: List<MovieEntity>)
 
-    @Update
-    suspend fun updateMovies(movies: List<MovieEntity>)
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertMoviesWithGenres(moviesWithGenres: List<MovieWithGenreCrossRef>)
 
     @Query("SELECT COUNT(*) from movieentity")
     suspend fun getNumberOfRecords(): Int
 
     @Transaction
     @Query("SELECT * from movieentity ORDER BY ratings DESC, title ASC")
-    suspend fun getMovies(): List<MovieWithGenre>
+    fun getMoviesWithGenre(): Flow<List<MovieWithGenre>>
+
+    fun getMoviesUntilChanged() = getMoviesWithGenre().distinctUntilChanged()
 }
