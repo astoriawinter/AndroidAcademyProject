a514c41c232f3f5b5e3d1b0cf8c2df9140c4b4f9
Tatiana Rodionova <tatiana.rodionova@emlid.com>
2021-02-07 20:48:45 +0300

    add MovieRepository logic & fixed previous bugs

--- app/src/main/java/com/tatiana/rodionova/androidacademyproject/data/repository/MovieRepository.kt
+++ app/src/main/java/com/tatiana/rodionova/androidacademyproject/data/repository/MovieRepository.kt
@@ -1,16 +1,26 @@
 package com.tatiana.rodionova.androidacademyproject.data.repository
 
+import android.content.Context
+import androidx.work.*
 import com.tatiana.rodionova.androidacademyproject.data.db.dao.ActorDao
 import com.tatiana.rodionova.androidacademyproject.data.db.dao.GenreDao
 import com.tatiana.rodionova.androidacademyproject.data.db.dao.MovieDao
 import com.tatiana.rodionova.androidacademyproject.data.db.dao.MovieDetailedDao
+import com.tatiana.rodionova.androidacademyproject.data.db.entity.MovieWithGenreCrossRef
 import com.tatiana.rodionova.androidacademyproject.data.db.mapper.*
 import com.tatiana.rodionova.androidacademyproject.data.network.MovieApi
+import com.tatiana.rodionova.androidacademyproject.data.service.MoviesLoaderWorker
 import com.tatiana.rodionova.androidacademyproject.model.Movie
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.withContext
+import org.koin.core.component.KoinApiExtension
+import java.util.concurrent.TimeUnit
 
 class MovieRepository(
+    private val context: Context,
     private val api: MovieApi,
     private val movieDao: MovieDao,
     private val genreDao: GenreDao,
@@ -18,31 +28,61 @@ class MovieRepository(
     private val movieDetailedDao: MovieDetailedDao
 ) {
 
-    suspend fun loadPopularMovies(): Flow<List<Movie>> = flow {
+    suspend fun loadPopularMovies(): Flow<List<Movie>> =
         if (isDatabaseEmpty()) {
-            val movies = getMovieFromApi()
-            emit(movies)
+            getMoviesFromApiAndUpdateDB()
+        } else getMoviesFromDBAndUpdateFromApi()
 
-            movieDao.insertMovies(movies.mapMovieToMovieEntity())
-            genreDao.insertGenres(movies.mapMovieToGenreEntity())
-        } else {
-            emit(movieDao.getMovies().toMovie())
+    private fun getMoviesFromApiAndUpdateDB(): Flow<List<Movie>> = flow {
+        val movies = getMovieFromApi()
+        emit(movies)
 
-            val movies = getMovieFromApi()
-            movieDao.updateMovies(movies.mapMovieToMovieEntity())
-            emit(movies)
-        }
+        movieDao.insertMovies(movies.mapMovieToMovieEntity())
+        genreDao.insertGenres(movies.mapMovieToGenreEntity())
+        movieDao.insertMoviesWithGenres(movies.flatMap { movie ->
+            movie.genres.map { genre ->
+                MovieWithGenreCrossRef(movieId = movie.id, genreId = genre.id)
+            }
+        })
     }
 
-    private suspend fun getMovieFromApi() =
+    suspend fun lazyDBUpdate() = withContext(Dispatchers.IO) {
+        val movies = getMovieFromApi()
+        movieDao.insertMovies(movies.mapMovieToMovieEntity())
+    }
+
+    @KoinApiExtension
+    fun setPeriodicMoviesLoadingTask() {
+        val constraints = Constraints.Builder()
+            .setRequiresCharging(true)
+            .setRequiredNetworkType(NetworkType.UNMETERED)
+            .build()
+
+        val periodicWork =
+            PeriodicWorkRequestBuilder<MoviesLoaderWorker>(UPDATE_PERIOD, TimeUnit.HOURS)
+                .setConstraints(constraints)
+                .build()
+
+        WorkManager.getInstance(context).enqueueUniquePeriodicWork(
+            LOAD_MOVIES_TASK,
+            ExistingPeriodicWorkPolicy.REPLACE,
+            periodicWork
+        )
+    }
+
+    private fun getMoviesFromDBAndUpdateFromApi(): Flow<List<Movie>> =
+        movieDao.getMoviesUntilChanged().map { movieWithGenre ->  movieWithGenre.toMovie() }
+
+    private suspend fun getMovieFromApi(): List<Movie> =
         api.getMovies().sortedWith(compareByDescending<Movie> { it.ratings }.thenBy { it.title })
 
-    private suspend fun isDatabaseEmpty(): Boolean = movieDao.getNumberOfRecords() <= 0
+    private suspend fun isDatabaseEmpty(): Boolean =
+        movieDao.getNumberOfRecords() <= 0
 
     private suspend fun isDetailedMovieIsEmpty(id: Long): Boolean =
         movieDetailedDao.getNumberOfRecords(id) <= 0
 
-    suspend fun loadMovie(id: Long): Flow<Movie> = flow {
+    fun loadMovie(id: Long): Flow<Movie> = flow {
         if (isDetailedMovieIsEmpty(id)) {
             api.getMovieById(id).let { movie ->
                 emit(movie)
@@ -54,7 +94,14 @@ class MovieRepository(
             emit(movieDetailedDao.getMovieDetailed(id).toMovie())
 
             val movie = api.getMovieById(id)
-            movieDetailedDao.updateMovieDetails(movie.mapToMovieDetailedEntity())
+            movieDetailedDao.insertMovieDetails(movie.mapToMovieDetailedEntity())
         }
+    }
+
+    companion object {
+
+        const val LOAD_MOVIES_TASK = "load movies"
+
+        const val UPDATE_PERIOD = 8L
     }
 }
